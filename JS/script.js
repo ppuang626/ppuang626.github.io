c3705f7be8f0e3a47e8b5ef86a6a35ecbeb87b5d
ppuang626 <puang_philippe@hotmail.fr>
2017-03-30 18:46:43 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,10 @@
-
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();

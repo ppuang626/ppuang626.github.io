93e0896f09a947da941a1bd8bd6de613cff23956
ppuang626 <puang_philippe@hotmail.fr>
2017-03-31 13:30:43 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,23 @@
+function webservice(url,id)
+{
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById(id).innerHTML = syntaxHighlight(jsonPretty);
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.open("GET", url, true);
 xmlhttp.send();
+}
+
+function homePageLoading() {
+    webservice("https://bitcoin.mubiz.com/info", "info");
+    
+    webservice("https://bitcoin.mubiz.com/blockchaininfo", "blockchaininfo");
+    
+    webservice("https://bitcoin.mubiz.com/peerinfo", "peerinfo");
+    
+    webservice("https://bitcoin.mubiz.com/mininginfo", "mininginfo");
+}

121ede005f1c46a06339d302ebd1dad53a46bb7e
ppuang626 <puang_philippe@hotmail.fr>
2017-04-30 12:17:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-function webservice(url,id)
-{
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById(id).innerHTML = syntaxHighlight(jsonPretty);
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+function webservice(url,id){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById(id).innerHTML = syntaxHighlight(jsonPretty);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
 
 function homePageLoading() {
